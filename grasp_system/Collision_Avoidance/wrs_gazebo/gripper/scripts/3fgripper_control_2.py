fa5e6eded6ae3cfa7a3f894ee6218f0e041f74dd
allen <zxcabc968123@gmail.com>
2020-04-14 15:56:54 +0800

    200414

--- grasp_system/Collision_Avoidance/wrs_gazebo/gripper/scripts/3fgripper_control_2.py
+++ grasp_system/Collision_Avoidance/wrs_gazebo/gripper/scripts/3fgripper_control_2.py
@@ -160,7 +160,7 @@ class Gripper:
             joint2_ang=self.get_gripper_state(3)
             joint3_ang=self.get_gripper_state(4)
             rospy.loginfo('Joint angle now = [ %.2f %.2f %.2f ]' ,joint1_ang,joint2_ang,joint3_ang)
-            while joint1_ang>-1.221:
+            while (joint1_ang>-1.221 and (self.l_Link3_bumper==False or self.r_Link3_bumper==False or self.m_Link3_bumper==False)):
 
                 pub1.publish(joint1_ang)
                 pub2.publish(joint2_ang)
@@ -173,16 +173,32 @@ class Gripper:
                 pub7.publish(joint1_ang)
                 pub8.publish(joint2_ang)
                 pub9.publish(joint3_ang)
-
+                ###
+                self.l_Link3_bumper=self.get_bumper_state('l_Link3_bumper')
+                self.r_Link3_bumper=self.get_bumper_state('r_Link3_bumper')
+                self.m_Link3_bumper=self.get_bumper_state('m_Link3_bumper')
+                ###
                 rospy.sleep(0.01)
                 joint1_ang=joint1_ang-0.01
                 joint3_ang=joint3_ang+0.01
-            while joint2_ang>-1.57:
+            force_ang=joint3_ang
+            for i in range(100):
+                joint3_ang=joint3_ang-0.01
+                pub3.publish(joint3_ang)
+                pub6.publish(joint3_ang)
+                pub9.publish(joint3_ang)
+                rospy.sleep(0.01)
+            while (joint2_ang>-1.57 and (self.l_Link3_bumper==False or self.r_Link3_bumper==False or self.m_Link3_bumper==False)):
                 pub2.publish(joint2_ang)
 
                 pub5.publish(joint2_ang)
 
                 pub8.publish(joint2_ang)
+                ###
+                self.l_Link3_bumper=self.get_bumper_state('l_Link3_bumper')
+                self.r_Link3_bumper=self.get_bumper_state('r_Link3_bumper')
+                self.m_Link3_bumper=self.get_bumper_state('m_Link3_bumper')
+                ###
 
                 rospy.sleep(0.01)
                 joint2_ang=joint2_ang-0.01
@@ -260,6 +276,7 @@ class Gripper:
                 rospy.sleep(0.01)
                 joint1_ang=joint1_ang-0.01
                 joint3_ang=joint3_ang+0.01
+            
             ##############
            
             while (joint2_ang>-1.57 and (self.l_Link3_bumper==False or self.r_Link3_bumper==False)):
@@ -270,8 +287,8 @@ class Gripper:
                 pub5.publish(joint2_ang)
                 
                 ###
-                self.get_bumper_state('l_Link3_bumper')
-                self.get_bumper_state('r_Link3_bumper')
+                self.l_Link3_bumper=self.get_bumper_state('l_Link3_bumper')
+                self.r_Link3_bumper=self.get_bumper_state('r_Link3_bumper')
                 ###
 
                 rospy.sleep(0.01)
@@ -312,6 +329,33 @@ class Gripper:
                 rospy.sleep(0.01)
                 joint1_ang=joint1_ang+0.01
                 joint3_ang=joint3_ang-0.01
+    def send_finger_reset(self):
+        pub1=rospy.Publisher(self.r_finger_joint1,Float64,queue_size=10)
+        pub2=rospy.Publisher(self.r_finger_joint2,Float64,queue_size=10)
+        pub3=rospy.Publisher(self.r_finger_joint3,Float64,queue_size=10)
+        pub4=rospy.Publisher(self.l_finger_joint1,Float64,queue_size=10)
+        pub5=rospy.Publisher(self.l_finger_joint2,Float64,queue_size=10)
+        pub6=rospy.Publisher(self.l_finger_joint3,Float64,queue_size=10)
+        pub7=rospy.Publisher(self.m_finger_joint1,Float64,queue_size=10)
+        pub8=rospy.Publisher(self.m_finger_joint2,Float64,queue_size=10)
+        pub9=rospy.Publisher(self.m_finger_joint3,Float64,queue_size=10)
+        #pub:publish名稱   chatter:topic name  queue_size=緩衝區大小
+        rate=rospy.Rate(self.publisher_rate)
+
+        pub1.publish(0)
+        pub2.publish(0)
+        pub3.publish(0)
+
+        pub4.publish(0)
+        pub5.publish(0)
+        pub6.publish(0)
+
+        pub7.publish(0)
+        pub8.publish(0)
+        pub9.publish(0)
+
+
+
 
 if __name__=='__main__':
     try:
@@ -322,12 +366,12 @@ if __name__=='__main__':
         # a.send_finger_one('m',2)
         # a.send_finger_one('l',1)
         # a.send_finger_one('l',2)
-        # a.send_rotating_command(0)
-        #a.send_finger_all(1)
+        #a.send_rotating_command(0)
+        a.send_finger_all(1)
         # a.send_finger_all(2)
-        a.send_rotating_command(1.57)
+        #a.send_rotating_command(1.57)
         #a.send_finger_two(1)
-        a.send_finger_two(1)
+        #a.send_finger_two(1)
         #a.send_finger_one('m',2)
         #a.send_finger_two(2)
         #a.send_rotating_command(0)
